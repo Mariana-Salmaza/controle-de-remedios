8c55b7b44f567b259724518760f482d3ba21c527
rsoliveeira <rodrigo_oliveeira@hotmail.com>
2025-03-19 01:29:42 -0300

    Atualizar UserModel.ts e adicionar .gitignore

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,11 +1,36 @@
 import { DataTypes, Model } from "sequelize";
 import sequelize from "../config/database";
+import bcrypt from "bcrypt";
 
-class UserModel extends Model {
+// Define os atributos do UserModel
+interface UserAttributes {
   id: number | undefined;
   name: string | undefined;
+  email: string | undefined;
+  document: string | undefined;
+  password: string | undefined;
 }
 
+// Define a classe UserModel
+class UserModel extends Model<UserAttributes> implements UserAttributes {
+  public id: number | undefined;
+  public name: string | undefined;
+  public email: string | undefined;
+  public document: string | undefined;
+  public password: string | undefined;
+
+  // Método para criptografar a senha
+  public async hashPassword() {
+    this.password = await bcrypt.hash(this.password!, 10);
+  }
+
+  // Método para validar a senha
+  public async validatePassword(password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password!);
+  }
+}
+
+// Inicialização do modelo
 UserModel.init(
   {
     id: {
@@ -17,6 +42,18 @@ UserModel.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    document: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
@@ -24,5 +61,16 @@ UserModel.init(
     tableName: "users",
   }
 );
+
+// Ganchos (Hooks) para criptografar a senha antes de criar ou atualizar o usuário
+UserModel.beforeCreate(async (user: UserModel) => {
+  await user.hashPassword();
+});
+
+UserModel.beforeUpdate(async (user: UserModel) => {
+  if (user.changed("password")) {
+    await user.hashPassword();
+  }
+});
 
 export default UserModel;
