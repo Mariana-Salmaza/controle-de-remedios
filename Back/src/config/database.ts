740c24f4b5f743500eafd4c93052c4b80111ee44
Mariana Salmaza <mariana.salmaza@grupointegrado.br>
2025-05-25 22:15:06 -0300

    feat: atualizando configuração do backend para usar novas variáveis de ambiente

--- Back/src/config/database.ts
+++ Back/src/config/database.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const isTest = process.env.NODE_ENV === "test";
 
 const sequelize = new Sequelize(
-  isTest ? "controle_remedios_test" : process.env.DB_NAME!,
+  isTest ? process.env.DB_NAME_TEST! : process.env.DB_NAME!,
   process.env.DB_USER!,
   process.env.DB_PASSWORD!,
   {
